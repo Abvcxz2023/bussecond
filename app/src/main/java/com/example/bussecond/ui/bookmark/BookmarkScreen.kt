a99628f041c30519cbce4ba39dd81f72828cb167
Abvcxz2023 <abvcxz2023@gmail.com>
2023-08-30 18:58:04 +0800

    small fix

--- app/src/main/java/com/example/bussecond/ui/bookmark/BookmarkScreen.kt
+++ app/src/main/java/com/example/bussecond/ui/bookmark/BookmarkScreen.kt
@@ -1,7 +1,9 @@
 package com.example.bussecond.ui.bookmark
 
 import android.util.Log
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,6 +25,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -29,10 +33,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bussecond.R
@@ -127,25 +135,46 @@ fun BookmarkScreen(
 fun MyTryItem(stopEtaList: List<StopEta>,
               bookmark: Bookmark = Bookmark("","",0,"","",""),
               onBookmarkDelete: (String, String, Int) -> Unit) {
-    Row(modifier = Modifier.wrapContentSize()) {
-        Column {
-            Text(text = "往${stopEtaList[0].dest_tc}")
-            Text(text = stopEtaList[0].route)
-            Text(text = bookmark.stopname)
-        }
-        Column {
-            for(etaData in stopEtaList){
-                Row {
-                    val eta: String = if(etaData.eta == null) {"-"} else if (etaData.eta == "-") {"-"} else { etaData.eta.substring(11,19)}
-                    Text(text = eta)
-                }
+    var expanded by remember{ mutableStateOf(false) }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .animateContentSize()
+        .clickable {
+            expanded = !expanded
+        }) {
+        Row {
+            Text(
+                text = "${stopEtaList[0].route}@${bookmark.stopname}",
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier
+                    .weight(9f)
+                    .padding(top = 4.dp, start = 8.dp),
+                maxLines = if (!expanded) 1 else Int.MAX_VALUE,
+                overflow = TextOverflow.Ellipsis)
+            IconButton(modifier = Modifier.weight(1f),onClick = {
+                onBookmarkDelete(stopEtaList[0].route, stopEtaList[0].dir, stopEtaList[0].seq)
+            }) {
+                Icon(imageVector = Icons.Default.Clear, contentDescription = "Delete")
             }
         }
-        Spacer(modifier = Modifier.weight(1f))
-        IconButton(onClick = {
-            onBookmarkDelete(stopEtaList[0].route,stopEtaList[0].dir,stopEtaList[0].seq)
-        }) {
-            Icon(imageVector = Icons.Default.Clear, contentDescription = "Delete")
+        Row {
+            Text(
+                text = "往${stopEtaList[0].dest_tc}",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+            )
+            Column(Modifier.fillMaxSize()) {
+                for (etaData in stopEtaList) {
+                    val eta: String = if (etaData.eta == null) {
+                        "-"
+                    } else if (etaData.eta == "-") {
+                        "-"
+                    } else {
+                        etaData.eta.substring(11, 19)
+                    }
+                    Text(text = eta, Modifier.align(Alignment.CenterHorizontally))
+                }
+            }
         }
     }
     Divider()
